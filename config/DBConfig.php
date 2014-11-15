a51d887cbfa1a339064f7cafa9ff468b0d9e9800
李波 <191358832@qq.com>
2014-11-15 14:03:45 +0800

    数据库配置文件
    补上数据库配置文件

---
+++ config/DBConfig.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * 数据库配置数组
+ */
+$config = array(
+	'mysql'	=>	array(
+		// 默认数据库
+		'default'	=> array(
+			// 线上数据库
+			'product'	=>	array(
+				'master'	=> array(
+					'host' 		=>	'127.0.0.1',
+					'username' 	=>	'root',
+					'password'	=>	'root',
+					'database'	=>	'cob',
+					'port'		=>	3306,
+					'charset'	=>	'UTF-8'
+				),
+				'slave'		=>	array(
+					'host' 		=>	'127.0.0.1',
+					'username' 	=>	'root',
+					'password'	=>	'root',
+					'database'	=>	'cob',
+					'port'		=>	3306,
+					'charset'	=>	'UTF-8'
+				)
+			),
+			
+			// 测试数据库
+			'test'	=>	array(),
+			// 开发数据库
+			'dev'	=>	array()
+		)
+	),
+	'mongo'	=>	array(
+		// 默认数据库
+		'default'	=> array(
+			// 线上数据库
+			'product'	=>	array(
+				'master'	=> array(
+					'host' 		=>	'127.0.0.1',
+					'username' 	=>	'',
+					'password'	=>	'',
+					'database'	=>	'',
+					'port'		=>	27017,
+					'charset'	=>	'UTF-8'
+				),
+				'slave'		=>	array(
+					'host' 		=>	'127.0.0.1',
+					'username' 	=>	'',
+					'password'	=>	'',
+					'database'	=>	'',
+					'port'		=>	27017,
+					'charset'	=>	'UTF-8'
+				)
+			),
+			
+			// 测试数据库
+			'test'	=>	array(),
+			// 开发数据库
+			'dev'	=>	array()
+		)
+	),
+		
+	'memcache'	=>	array(),
+	'radis'	=>	array()
+	
+);
+
+
+class DBConfig {
+	
+	public static $config = array();
+	
+	public static function set($config)
+	{
+		self::$config = $config;
+	}
+}
+
+DBConfig::set($config);
+
